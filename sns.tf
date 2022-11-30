5bdc8e7de2f15f995e5147a6374dc25c4085e418
eelayoubi <ezzeddine.el.ayoubi@gmail.com>
2022-11-30 16:26:26 +0100

    add sns permission to invoke lambda

--- sns.tf
+++ sns.tf
@@ -7,3 +7,11 @@ resource "aws_sns_topic_subscription" "convert_post_subscription" {
   protocol  = "lambda"
   endpoint  = module.convert_post_to_audio_lambda.function_arn
 }
+
+resource "aws_lambda_permission" "allow_sns_invoke" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = module.convert_post_to_audio_lambda.function_name
+  principal     = "sns.amazonaws.com"
+  source_arn    = aws_sns_topic.new_posts.arn
+}
